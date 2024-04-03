97de9c484457421f3cd10c37a41f7cb37b36744c
Gustavo Adolpho Bonesso <gustavo_bonesso@hotmail.com>
2024-04-03 12:50:39 -0300

    Adiciona seleção de imagem do celular e página de configurações de detecção

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:recon_loc_caract_faces/detection_settings_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Reconhecimento e Localização de Características Faciais',
+      home: MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -9,10 +17,30 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return SafeArea(
+      child: Scaffold(
+        body: ListView(
+          children: <Widget>[
+            ListTile(
+              title: Text('Selecione imagem do celular'),
+              onTap: () async {
+                final ImagePicker picker = ImagePicker();
+                // Pick an image.
+                final XFile? image =
+                    await picker.pickImage(source: ImageSource.gallery);
+                if (image != null) {
+                  print(image.path);
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) => Dialog.fullscreen(
+                      child: DetectionSettingsPage(imagePath: image.path),
+                    ),
+                  );
+                }
+              },
+            ),
+            Divider()
+          ],
         ),
       ),
     );
